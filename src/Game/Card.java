f92e78e2a895eaf29f0b975ea712b6527a90c9aa
kodie03 <158498269+kodie03@users.noreply.github.com>
2025-11-19 21:32:57 -0800

    Update Card.java

--- src/Game/Card.java
+++ src/Game/Card.java
@@ -1,1 +1,24 @@
+public class Card {
+    private final Rank rank;  
+    private final Suit suit; 
 
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+// Getters
+    public Rank getRank() {
+        return rank;
+    }
+    public Suit getSuit() {
+        return suit;
+    }
+    public int getCardValue() {
+        return rank.getValue();
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
