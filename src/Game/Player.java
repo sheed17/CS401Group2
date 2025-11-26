a3a74ea93efe79dab94863933b87aab095437841
AlexisChJ <alexchj1917@gmail.com>
2025-11-25 20:11:31 -0800

    log in finished, with txt file reading it

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Player {
 	
 	private int userId;
@@ -20,14 +23,58 @@ public class Player {
 	}
 	
 	public void logIn(String u, String p) {
-		if (u == this.username && p == this.password) {
-			return;
+	//CHANGE FILE NAME WHEN WE DEFINE THAT	
+	// I ASSUMED THAT THE FILE HAS THIS FORMAT
+	// USER,PASSWORD
+		File file = new File("users.txt");   
+		
+		if (file.exists() == false) {
+		    return;
 		}
-		break;
+		
+		boolean found = false;
+		
+		try {
+		    Scanner scanner = new Scanner(file);
+		    while (scanner.hasNextLine()) {
+		        String line = scanner.nextLine().trim();
+		        if (line.isEmpty()) {
+		            continue;
+		        }
+		
+		        String[] userData = line.split(",");
+		        if (userData.length != 2) {
+		            break;
+		        }
+		
+		        String fileUser = userData[0].trim();
+		        String filePass = userData[1].trim();
+		
+		        if (fileUser.isBlank() || filePass.isBlank()) {
+		            break;
+		        }
+		
+		        if (u.equals(fileUser) && p.equals(filePass)) {
+		            this.username = fileUser;
+		            this.password = filePass;
+		            found = true;
+		            break;
+		        }
+		    }
+		    scanner.close();
+		} catch (Exception e) {
+			//ONLY TO TEST, WE NEED TO GET RID OF THIS LATER 
+		    System.out.println("ERROR WHILE READING FILE");
+		}
+		
+		if (!found) {
+		    // incorrect credentials
+		}
 	}
+
 	
 	public void logout() {
-		
+		//missing 
 	}
 	
 	public void joinTable(Table table) {
@@ -103,7 +150,7 @@ public class Player {
 	}
 	
 	public void split() {
-		
+		//missing 
 	}
 	
 	public int getUserId() {
