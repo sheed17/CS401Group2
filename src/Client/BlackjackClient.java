e6bcdc6f0a7c5db1755ab3931a512ecf58909859
luisaguilar5109 <123496869+luisaguilar5109@users.noreply.github.com>
2025-11-28 15:27:09 -0800

    Updated file location

--- src/Client/BlackjackClient.java
+++ src/Client/BlackjackClient.java
@@ -1,52 +1,87 @@
-package src.Client;
 import java.net.*;
 import java.util.*;
+
+import javax.swing.JOptionPane;
+
 import java.io.*;
 
 public class BlackjackClient
 {
+	private static boolean loggedIn = false;
+	private Socket socket;
+	private ObjectInputStream objectIn;
+	private ObjectOutputStream objectOut;
+
+	public BlackjackClient() throws IOException
+	{
+		this.socket = new Socket("localhost", 52904);
+		this.objectOut = new ObjectOutputStream(socket.getOutputStream());
+		objectOut.flush();
+		this.objectIn = new ObjectInputStream(socket.getInputStream());
+	}
+	
+	
 	public static void main(String[] args) throws IOException
 	{
-		String IPAddress;
-		// This is temporary
-		int portNumber;
-		boolean loggedIn = false;
+		BlackjackClient client = new BlackjackClient();
+		// Creates a new client object to be passed into the GUI (not needed for now)
+		// Might not be needed in general, commenting out for now as it messes with the method references
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the IP Address of the Server: ");
-		IPAddress = scan.nextLine().trim();
-		
-		System.out.println("Enter the port number of the server: ");
-		portNumber = scan.nextInt();
-		scan.nextLine();
-		
-		try (Socket socket = new Socket(IPAddress, portNumber))
-		{
-			var input = new Scanner(socket.getInputStream());
-			var output = new PrintWriter(socket.getOutputStream(), true);
-			System.out.println(input.nextLine());
-
-			// Test for logging into the server;
-			while (loggedIn == false)
-			{
-				System.out.println("Input username: ");
-				String username = scan.nextLine().trim();
-				output.println(username);
-		
-				System.out.println("Enter your password: ");
-				String password = scan.nextLine().trim();
-				output.println(password);
-			
-				String serverMessage = input.nextLine().trim();
-				if (serverMessage.contains("You are now logged in"))
-				{
-					System.out.println(serverMessage);
-					loggedIn = true;
-				}
-				else 
-					System.out.println(serverMessage);
-				
-			}
-		}
+		GameGUI newGUI = new GameGUI(client);
+		newGUI.logInGUI();
 	}
+	
+	public synchronized void send(BlackjackMessage message)
+	{
+		try
+		{
+			objectOut.writeObject(message);
+			objectOut.flush();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public synchronized BlackjackMessage receive()
+	{
+		try {
+            return (BlackjackMessage) objectIn.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+	}
+	
+	
+	public String logIn(String username, String password)
+	{
+		send(new BlackjackMessage(MessageEnum.LOGIN,username,password));	
+		BlackjackMessage message = receive();
+		if (message.getStatus().equals("Success"))
+			loggedIn=true;
+		return message.getStatus();
+	}
+	// GUI version of logIn() method
+		
+	public String createNewUser(String username, String password)
+	{
+		send(new BlackjackMessage(MessageEnum.NEWUSER,username,password));
+		BlackjackMessage message = receive();
+		if (message.getStatus().equals("Success"))
+			loggedIn = true;
+		return message.getStatus();
+	}
+	// GUI version of CreateNewUser() method	
+		
+	public void updateBalance()
+	{
+		send(new BlackjackMessage(MessageEnum.UPDATEBALANCE, "add", "150"));
+		BlackjackMessage serverResponse = receive();
+		System.out.println(serverResponse.getText());
+		// Basic example of sending a message from the client to the server
+	}
+	
 }
