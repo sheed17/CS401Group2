6f6d8dde9e2625afd5545b8ada0ab2c649670e26
luisaguilar5109 <123496869+luisaguilar5109@users.noreply.github.com>
2025-11-28 14:59:17 -0800

    NewUserGUI Created
    Finished the GUI window for creating a new user account. Additionally, removed the old command line interface methods for mainMenu(), logIn(), and CreateNewUser() and replaced with the new GUI versions. Will work on creating the basic mainMenu GUI that features a view profile button, view tables button and log out button but the view profile and view tables button implementation will be left to the people behind those classes.

--- BlackjackClient.java
+++ BlackjackClient.java
@@ -1,5 +1,8 @@
 import java.net.*;
 import java.util.*;
+
+import javax.swing.JOptionPane;
+
 import java.io.*;
 
 public class BlackjackClient
@@ -20,33 +23,12 @@ public class BlackjackClient
 	
 	public static void main(String[] args) throws IOException
 	{
-		Scanner userInput = new Scanner(System.in);
-		// Used for receiving command line user input
-		
 		BlackjackClient client = new BlackjackClient();
 		// Creates a new client object to be passed into the GUI (not needed for now)
 		// Might not be needed in general, commenting out for now as it messes with the method references
 		
-		int logInChoice;
-		// used to determine if the user is returning or new and then calls the corresponding function
-		
-		//GameGUI newGUI = new GameGUI(client);
-		
-		do 
-		{
-			System.out.println("Welcome to the Blackjack Game!"
-					+ "\nEnter 1 to log in or 2 to register a new account");
-			logInChoice = userInput.nextInt();
-			userInput.nextLine();
-		
-		} while (logInChoice != 1 && logInChoice != 2);
-		
-		switch (logInChoice)
-		{
-			case 1: client.logIn(userInput); break;
-			case 2: client.createNewUser(userInput); break;
-		}
-		client.mainMenu();
+		GameGUI newGUI = new GameGUI(client);
+		newGUI.logInGUI();
 	}
 	
 	public synchronized void send(BlackjackMessage message)
@@ -73,61 +55,33 @@ public class BlackjackClient
         }
 	}
 	
-	public void logIn(Scanner userInput)
-	{
-		while (!loggedIn)
-		{
-			System.out.println("Enter your username");
-			String username = userInput.nextLine();
-			
-			System.out.println("Enter your password");
-			String password = userInput.nextLine();
-			
-			send(new BlackjackMessage(MessageEnum.LOGIN,username,password));
-			
-			BlackjackMessage message = receive();
-			if (message.getStatus().equals("Success"))
-				loggedIn=true;
-			System.out.println(message.getText());
-		}
-	}
 	
-	public void createNewUser(Scanner userInput)
+	public String logIn(String username, String password)
 	{
-		while (!loggedIn)
-		{
-			System.out.println("Enter your username");
-			String username = userInput.nextLine();
-			
-			System.out.println("Enter your password");
-			String password = userInput.nextLine();
-			
-			System.out.println("You entered: \nUsername: " + username + "\nPassword: " + password + "\nIs this okay? (Y/N)");
-			String confirmation = userInput.nextLine().trim();
-			
-			if (confirmation.equalsIgnoreCase("Y"))
-			{
-				send(new BlackjackMessage(MessageEnum.NEWUSER,username,password));
-				BlackjackMessage serverResponse = receive();
-				if (serverResponse.getStatus().equals("Success"))
-				{
-					loggedIn = true;
-				}
-				System.out.println(serverResponse.getText());
-			}
-			
-		}
-		// Luis' idea: User enters username and password. Gets asked for confirmation since its a new account. If
-		// the user is sure of their details, a message is sent to the server with the details. Not implemented yet, but the server
-		// should search the user data files and make sure the username is unique before registering their details. If its unique, 
-		// server returns a message saying that they've been registered successfully, otherwise a message is sent back saying that they
-		// gotta choose a different username. 
+		send(new BlackjackMessage(MessageEnum.LOGIN,username,password));	
+		BlackjackMessage message = receive();
+		if (message.getStatus().equals("Success"))
+			loggedIn=true;
+		return message.getStatus();
 	}
-	
-	public void mainMenu()
+	// GUI version of logIn() method
+		
+	public String createNewUser(String username, String password)
 	{
-		send(new BlackjackMessage(MessageEnum.UPDATEBALANCE,"add", Integer.toString(150)));
+		send(new BlackjackMessage(MessageEnum.NEWUSER,username,password));
+		BlackjackMessage message = receive();
+		if (message.getStatus().equals("Success"))
+			loggedIn = true;
+		return message.getStatus();
+	}
+	// GUI version of CreateNewUser() method	
+		
+	public void updateBalance()
+	{
+		send(new BlackjackMessage(MessageEnum.UPDATEBALANCE, "add", "150"));
 		BlackjackMessage serverResponse = receive();
 		System.out.println(serverResponse.getText());
+		// Basic example of sending a message from the client to the server
 	}
+	
 }
