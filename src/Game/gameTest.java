ef7a5fde42bc891440747fd6a49fa547e9a8db93
luisaguilar5109 <123496869+luisaguilar5109@users.noreply.github.com>
2025-11-30 21:43:37 -0800

    gameTest.java for connecting game

---
+++ src/Game/gameTest.java
@@ -0,0 +1,13 @@
+package src.Game;
+
+public class gameTest
+{
+	
+	public static void main(String[] args)
+	{
+		Table testTable = new Table();
+		Player tester = new Player("testuser", "testpassword");
+		testTable.addPlayer(tester);
+		
+	}
+}
